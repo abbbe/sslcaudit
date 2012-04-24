387fd36348057d6cffe2038b70598a20cf18c33a
Alexandre Bezroutchko <abb@gremwell.com>
2012-04-25 00:09:14 +0200

    Grab the local socket address immediately. Otherwise it might be available later on for logging purposes, when the socket is closed.

--- src/ClientAuditor/ClientConnection.py
+++ src/ClientAuditor/ClientConnection.py
@@ -2,6 +2,7 @@ class ClientConnection(object):
     def __init__(self, sock, client_address):
         self.sock = sock
         self.client_address = client_address
+        self.sockname = self.sock.getsockname()
 
     def get_client_id(self):
         '''
@@ -11,4 +12,4 @@ class ClientConnection(object):
         return self.client_address[0]
 
     def __repr__(self):
-        return "%s [%s->%s]" % (self.get_client_id(), self.client_address, self.sock.getsockname())
+        return "%s [%s->%s]" % (self.get_client_id(), self.client_address, self.sockname)
