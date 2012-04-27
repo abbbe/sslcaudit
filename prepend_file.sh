d5c92799403cbce6e91d89dcd29ef35677600076
Alexandre Bezroutchko <abb@gremwell.com>
2012-04-27 20:01:01 +0200

    more gpl

---
+++ prepend_file.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -xe
+
+# ----------------------------------------------------------------------------
+# SSLCAUDIT - a tool for automatingsecurity audit of SSL clients
+# Released under terms of GPLv3, see COPYING.TXT 
+# Copyright (C) 2012 Alexandre Bezroutchko abb@gremwell.com
+# ----------------------------------------------------------------------------
+
+# After execution of "./prepend.sh F1 F2" file F2 will contain F2 + F1"
+
+[ $# -eq 2 ]
+
+mv "$1" "$1.orig"
+cat "$2" "$1.orig" > "$1"
+rm "$1.orig"
+
