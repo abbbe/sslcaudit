1ed903f7e38a9b634c4fd38ca36e8ee18334812e
Alexandre Bezroutchko <abb@gremwell.com>
2012-04-27 20:08:26 +0200

    fixed a typo in the banner

--- test/TestMainDummy.py
+++ test/TestMainDummy.py
@@ -1,5 +1,5 @@
 ''' ----------------------------------------------------------------------
-SSLCAUDIT - a tool for automatingsecurity audit of SSL clients
+SSLCAUDIT - a tool for automating security audit of SSL clients
 Released under terms of GPLv3, see COPYING.TXT
 Copyright (C) 2012 Alexandre Bezroutchko abb@gremwell.com
 ---------------------------------------------------------------------- '''
