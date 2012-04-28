d22a9d4fa93e9f74f4f539a8cffd1dad5d8bf96a
Alexandre Bezroutchko <abb@gremwell.com>
2012-04-28 16:06:17 +0200

    added gpl banner to shell scripts, taken m2cryptobug unittest out of the normally executed set of unittests

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 #!/bin/sh -x
 
+# ----------------------------------------------------------------------
+# SSLCAUDIT - a tool for automating security audit of SSL clients
+# Released under terms of GPLv3, see COPYING.TXT
+# Copyright (C) 2012 Alexandre Bezroutchko abb@gremwell.com
+# ----------------------------------------------------------------------
+
 env PYTHONPATH="`dirname \"$0\"`" python src/Main.py $*
--- test.sh
+++ test.sh
@@ -1,5 +1,11 @@
 #!/bin/sh -x
 
-for m in TestMainDummy TestMainSSL ; do
-	env PYTHONPATH="`dirname \"$0\"`" python test/$m.py
+# ----------------------------------------------------------------------
+# SSLCAUDIT - a tool for automating security audit of SSL clients
+# Released under terms of GPLv3, see COPYING.TXT
+# Copyright (C) 2012 Alexandre Bezroutchko abb@gremwell.com
+# ----------------------------------------------------------------------
+
+for f in test/Test*.py ; do
+	env PYTHONPATH="`dirname \"$0\"`" python $f
 done
