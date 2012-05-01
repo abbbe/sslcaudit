7d4d4c46e39db5723df3689355642847dbf9fca9
Alexandre Bezroutchko <abb@gremwell.com>
2012-05-01 16:32:01 +0200

    cleanup

--- src/ClientAuditor/SSL/__init__.py
+++ src/ClientAuditor/SSL/__init__.py
@@ -3,5 +3,3 @@ SSLCAUDIT - a tool for automating security audit of SSL clients
 Released under terms of GPLv3, see COPYING.TXT
 Copyright (C) 2012 Alexandre Bezroutchko abb@gremwell.com
 ---------------------------------------------------------------------- '''
-
-MODULE_ID='sslcert'
