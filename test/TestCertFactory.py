cb3b54ffa23097e93eb2b898df7dd47163fb6b07
Alexandre Bezroutchko <abb@gremwell.com>
2012-05-01 12:57:28 +0200

    fixed unit test

--- test/TestCertFactory.py
+++ test/TestCertFactory.py
@@ -28,13 +28,17 @@ class TestCertFactory(unittest.TestCase):
         self.assertEqual(good_subj, certnkey.cert.get_subject().as_text())
         self.assertEqual(certnkey.cert.get_issuer().as_text(), ca_certnkey.cert.get_subject().as_text())
 
-    def test_mk_server_replica_cert(self):
+    def test_mk_selfsigned_server_replica_cert(self):
         '''
-        This test grabs a certificate from the test server and
+        This test grabs a certificate from the test server and self-signs it
         '''
         server_cert = self.cert_factory.grab_server_x509_cert((TEST_SERVER_HOST, TEST_SERVER_PORT))
-        ss_replica_certnkey = self.cert_factory.mk_selfsigned_replica_certnkey(server_cert)
+        ss_replica_certnkey = self.cert_factory.mk_signed_replica_certnkey(server_cert)
         self.assertEqual(server_cert.get_subject().as_text(), ss_replica_certnkey.cert.get_subject().as_text())
+
+    def test_mk_signed_server_replica_cert(self):
+        # XXX
+        pass
 
     def test_grab_server_x509_cert1(self):
         self.cert_factory.grab_server_x509_cert((TEST_SERVER_HOST, TEST_SERVER_PORT))
