cf1d09d192810351b16d6993d0243b4e7eb8c10f
Evgeniy Shubin <ev.shuba@gmail.com>
2012-05-09 18:04:08 +0400

    first version of packet

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from distutils.core import setup
 
 setup(name='sslcaudit',
+      url='https://github.com/grwl/sslcaudit',
       version='1.0',
+      license = "GPL3",
       scripts=['sslcaudit'],
+      data_files=['test-sslcaudit'],
       package_dir={'sslcaudit': 'src'},
       packages = ['sslcaudit', 'sslcaudit.core', 'sslcaudit.modules',
 		  'sslcaudit.modules.base', 'sslcaudit.modules.dummy',
