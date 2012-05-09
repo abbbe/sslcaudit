f193349215ade3cdebedf637f7cee084fd4a5f2a
Alexandre Bezroutchko <abb@gremwell.com>
2012-05-09 15:41:25 +0200

    improved error handling; changed -l parameter format to HOST:PORT (now host is mandatory); now resolve service names in -l and --server address specifications

--- src/core/ConfigError.py
+++ src/core/ConfigError.py
@@ -4,7 +4,7 @@ Released under terms of GPLv3, see COPYING.TXT
 Copyright (C) 2012 Alexandre Bezroutchko abb@gremwell.com
 ---------------------------------------------------------------------- '''
 
-class ConfigErrorException(Exception):
+class ConfigError(Exception):
     '''
     The code has to throw this exception if it has a problem with input
     parameters. Should only be thrown during startup.
