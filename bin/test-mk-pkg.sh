adf2deff02e27dacbbcf75a5e142b4ee039f7b76
Alexandre Bezroutchko <abb@gremwell.com>
2012-05-13 09:51:38 +0200

    cleanup

--- bin/test-mk-pkg.sh
+++ bin/test-mk-pkg.sh
@@ -10,7 +10,11 @@
 # This script rebiulds .deb package, (re)installs in locally, and lists
 # files deployed by the package.
 
-./create_deb.sh
+bin/create_deb.sh
 sudo dpkg -i deb_dist/python-sslcaudit_1.0-1_all.deb 
 dpkg -L python-sslcaudit
+
+cd /tmp
+which sslcaudit
+sslcaudit -T 1 --user-cn localhost
 
