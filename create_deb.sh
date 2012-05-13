e502610c7147715067df7096ae136b22636665a0
Alexandre Bezroutchko <abb@gremwell.com>
2012-05-13 09:13:53 +0200

    added license header and other cosmetic changes to distribution scripts

--- create_deb.sh
+++ create_deb.sh
@@ -1,1 +1,9 @@
+#!/bin/sh -xe
+
+# ----------------------------------------------------------------------
+# SSLCAUDIT - a tool for automating security audit of SSL clients
+# Released under terms of GPLv3, see COPYING.TXT
+# Copyright (C) 2012 Alexandre Bezroutchko abb@gremwell.com
+# ----------------------------------------------------------------------
+
 python setup.py --command-packages=stdeb.command bdist_deb
--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
+# ----------------------------------------------------------------------
+# SSLCAUDIT - a tool for automating security audit of SSL clients
+# Released under terms of GPLv3, see COPYING.TXT
+# Copyright (C) 2012 Alexandre Bezroutchko abb@gremwell.com
+# ----------------------------------------------------------------------
+
 from distutils.core import setup
 
 setup(name='sslcaudit',
-      url='https://github.com/grwl/sslcaudit',
-      version='1.0',
-      license = "GPL3",
-      scripts=['sslcaudit'],
-      package_dir={'src': 'src'},
-      packages = ['src', 'src.core', 'src.modules',
-		  'src.modules.base', 'src.modules.dummy',
-		  'src.modules.sslcert', 'src.profile', 'src.test']
-      )
+    url='http://www.gremwell.com/sslcaudit_v1_0',
+    version='1.0',
+    license='GPLv3',
+    scripts=['sslcaudit'],
+    package_dir={'src': 'src'},
+    packages=['src', 'src.core', 'src.modules',
+              'src.modules.base', 'src.modules.dummy',
+              'src.modules.sslcert', 'src.profile', 'src.test']
+)
