67b2af218b852546f7edcb39373ba64eac9ddd93
Evgeniy Shubin <shubin@reksoft.ru>
2012-05-14 12:16:04 -0400

    custom debian packaging

--- bin/mk-deb.sh
+++ bin/mk-deb.sh
@@ -1,11 +1,1 @@
-#!/bin/sh -xe
-
-# ----------------------------------------------------------------------
-# SSLCAUDIT - a tool for automating security audit of SSL clients
-# Released under terms of GPLv3, see COPYING.TXT
-# Copyright (C) 2012 Alexandre Bezroutchko abb@gremwell.com
-# ----------------------------------------------------------------------
-
-python setup.py --command-packages=stdeb.command bdist_deb
-
-dpkg -I deb_dist/python-sslcaudit_1.0-1_all.deb
+dpkg-buildpackage -rfakeroot -uc -us
