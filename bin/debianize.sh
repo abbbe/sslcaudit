bbd0b14ff920aed0fc795e8a076ff1214ded1ee4
Alexandre Bezroutchko <abb@gremwell.com>
2012-05-15 19:32:54 +0200

    comments and changes to source code format id

--- bin/debianize.sh
+++ bin/debianize.sh
@@ -6,13 +6,23 @@
 # Copyright (C) 2012 Alexandre Bezroutchko abb@gremwell.com
 # ----------------------------------------------------------------------
 
+# drop debian/
 rm -rf debian
+
+# rebuild debian/
 python setup.py --command-packages=stdeb.command debianize
 
+# copy the licence file
 cp COPYING.TXT debian/copyright
+
+# fix dependency (why 'debianize' does not do it?)
 patch debian/control <<'END'
 10c10
 < Depends: ${misc:Depends}, ${python:Depends}
 ---
 > Depends: ${misc:Depends}, ${python:Depends}, python-m2crypto
 END
+
+# fix source code format
+echo 1.0 > debian/source/format
+
