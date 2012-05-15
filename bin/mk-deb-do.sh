0a5bda3d929c7842f0e64532e8411587ed6e2383
Alexandre Bezroutchko <abb@gremwell.com>
2012-05-15 21:12:22 +0200

    cherrypicking

--- bin/mk-deb-do.sh
+++ bin/mk-deb-do.sh
@@ -1,1 +1,14 @@
-dpkg-buildpackage -rfakeroot -uc -us -tc
+#!/bin/sh -xe
+
+# ----------------------------------------------------------------------
+# SSLCAUDIT - a tool for automating security audit of SSL clients
+# Released under terms of GPLv3, see COPYING.TXT
+# Copyright (C) 2012 Alexandre Bezroutchko abb@gremwell.com
+# ----------------------------------------------------------------------
+
+# copy the licence file
+cp COPYING.TXT debian/copyright
+
+# build .deb file
+dpkg-buildpackage -A -rfakeroot -uc -us -tc
+
