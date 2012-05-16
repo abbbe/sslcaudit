e2f4eb50ce0786e31f28e8917c1d4b7597b7b865
Nikita Nikishin <nnikishi@emich.edu>
2012-05-15 20:13:00 -0400

    Implemented GUI into the code.

--- sslcaudit/core/SSLCAuditGUI.py
+++ sslcaudit/core/SSLCAuditGUI.py
@@ -5,7 +5,11 @@ Copyright (C) 2012 Alexandre Bezroutchko abb@gremwell.com
 ---------------------------------------------------------------------- '''
 
 import sys, logging
-from PyQt4 import QtGui
+
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+
+import SSLCauditGUIGenerated
 
 logger = logging.getLogger('SSLCAuditGUI')
 
@@ -16,13 +20,33 @@ class SSLCAuditGUI(object):
         '''
         self.options = options
 
-        # initialize dummy GUI
-        self.app = QtGui.QApplication(sys.argv)
-        self.w = QtGui.QWidget()
-        self.w.resize(250, 150)
-        self.w.move(300, 300)
-        self.w.setWindowTitle('Simple')
-        self.w.show()
+        self.app = QApplication(sys.argv)
+        self.window = SSLCauditGUIWindow(self.options)
 
     def run(self):
+        self.window.show()
         return self.app.exec_()
+
+
+class SSLCauditGUIWindow(QMainWindow):
+    def __init__(self, options, parent=None):
+        '''
+        Initialize UI. Dictionary 'options' comes from SSLCAuditUI.parse_options().
+        '''
+        QMainWindow.__init__(self, parent)
+        
+        self.options = options
+
+        self.ui = SSLCauditGUIGenerated.Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.ui.startButton.setIcon(QIcon.fromTheme('media-playback-start'))
+        
+        for control in [
+          self.ui.certificateBrowse1,
+          self.ui.certificateBrowse2,
+          self.ui.keyBrowse1,
+          self.ui.keyBrowse2
+        ]:
+          control.setIcon(QIcon.fromTheme('document-open'))
+          
