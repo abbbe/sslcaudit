51c35d07265ecb935403761070a12518bce92008
Alexandre Bezroutchko <abb@gremwell.com>
2012-05-22 02:07:09 +0200

    fixed type of -v and -c parameters

--- sslcaudit/core/SSLCAuditUI.py
+++ sslcaudit/core/SSLCAuditUI.py
@@ -23,11 +23,11 @@ def parse_options(argv):
         + "new modules. Default is %s" % DEFAULT_MODULES)
     parser.add_option("-g", dest="gui", action="store_true", default=False,
         help="Use graphical UI")
-    parser.add_option("-v", dest="verbose", default=0,
+    parser.add_option("-v", type='int', dest="verbose", default=0,
         help="Increase verbosity level. Default is 0. Try 1.")
     parser.add_option("-d", type='int', dest="debug_level", default=0,
         help="Set debug level. Default is 0, which disables debugging output. Try 1 to enable it.")
-    parser.add_option("-c", dest="nclients", default=1,
+    parser.add_option("-c", type='int', dest="nclients", default=1,
         help="Number of clients to handle before quitting. By default sslcaudit will quit as soon as "
         + "it gets one client fully processed.")
     parser.add_option("-N", dest="test_name",
