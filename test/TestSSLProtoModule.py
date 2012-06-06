ed8c15845165f6e75df42aafa553841805a71e89
Alexandre Bezroutchko <abb@gremwell.com>
2012-06-07 00:39:01 +0200

    implemented one more method for sslproto module unittest

--- test/TestSSLProtoModule.py
+++ test/TestSSLProtoModule.py
@@ -9,7 +9,8 @@ import logging, unittest
 from sslcaudit.modules.sslcert.ProfileFactory import DEFAULT_CN, SSLProfileSpec_SelfSigned, SSLProfileSpec_IMCA_Signed, SSLProfileSpec_Signed, IM_CA_FALSE_CN, IM_CA_TRUE_CN, IM_CA_NONE_CN, SSLProfileSpec_UserSupplied
 from sslcaudit.modules.sslcert.SSLServerHandler import     UNEXPECTED_EOF, ALERT_UNKNOWN_CA, ConnectedGotEOFBeforeTimeout, ConnectedGotRequest
 from sslcaudit.modules.sslproto.ProfileFactory import SSLServerProtoSpec
-from sslcaudit.test.ExternalCommandHammer import CurlHammer
+from sslcaudit.modules.sslproto.ServerHandler import Connected
+from sslcaudit.test.ExternalCommandHammer import CurlHammer, OpenSSLHammer
 from sslcaudit.test.TCPConnectionHammer import TCPConnectionHammer
 from sslcaudit.test.TestConfig import *
 from test import TestModule
@@ -40,20 +41,37 @@ class TestSSLProtoModule(TestModule.TestModule):
             eccars
         )
 
-    def test_curl(self):
+    def test_curl_weak_ciphers(self):
         # curl (and any other proper SSL client for that purpose) is expected to reject SSLv2 and weak ciphers
         eccars = []
         for proto in ('sslv23',):
             for cipher in ('HIGH', 'MEDIUM', 'LOW', 'EXPORT'):
                 if cipher == 'HIGH' or cipher == 'MEDIUM':
+                    # we expect curl to establish the connection to a server offering a reasonably strong cipher,
+                    # but complain about CA (we didn't bother specifying it)
                     expected_res = ALERT_UNKNOWN_CA
                 else:
+                    # we expect curl to refuse connecting to server offering weak ciphers
                     expected_res = ALERT_NO_SHARED_CIPHER
                 eccars.append(ECCAR(SSLServerProtoSpec(proto, cipher), expected_res=expected_res))
 
         self._main_test(
             ['-m', 'sslproto'],
             CurlHammer(len(eccars)),
+            eccars
+        )
+
+    def test_opensssl_all_ciphers(self):
+        # openssl client is expected to connect to anything
+        eccars = []
+        for proto in ('sslv23',):
+            for cipher in ('HIGH', 'MEDIUM', 'LOW', 'EXPORT'):
+                expected_res = Connected()
+                eccars.append(ECCAR(SSLServerProtoSpec(proto, cipher), expected_res=expected_res))
+
+        self._main_test(
+            ['-m', 'sslproto'],
+            OpenSSLHammer(len(eccars)),
             eccars
         )
 
