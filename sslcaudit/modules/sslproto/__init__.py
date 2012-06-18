61b04e665ac24566d157160a09f24d905428de37
Miroslav Stampar <miroslav.stampar@gmail.com>
2012-06-18 12:50:49 +0200

    Patch for an Issue #44

--- sslcaudit/modules/sslproto/__init__.py
+++ sslcaudit/modules/sslproto/__init__.py
@@ -3,7 +3,10 @@
 # Released under terms of GPLv3, see COPYING.TXT
 # Copyright (C) 2012 Alexandre Bezroutchko abb@gremwell.com
 # ----------------------------------------------------------------------
+import M2Crypto
 
-PROTOCOLS = ('sslv2', 'sslv3', 'tlsv1')
+IS_SSLv2_SUPPORTED = hasattr(M2Crypto.m2, "sslv2_method") and M2Crypto.m2.ssl_ctx_new(M2Crypto.m2.sslv2_method()) is not None
+
+PROTOCOLS = filter(None, ('sslv2' if IS_SSLv2_SUPPORTED else None, 'sslv3', 'tlsv1'))
 EXPORT_CIPHER = 'EXPORT'
 CIPHERS = ('HIGH', 'MEDIUM', 'LOW', EXPORT_CIPHER)
