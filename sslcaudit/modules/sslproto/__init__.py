4369e82c16977feac4d18bb871ae72cadebde397
Alexandre Bezroutchko <abb@gremwell.com>
2012-06-19 13:19:55 +0200

    minor refactoring of SSlv2 detection. added alerting

--- sslcaudit/modules/sslproto/__init__.py
+++ sslcaudit/modules/sslproto/__init__.py
@@ -4,9 +4,15 @@
 # Copyright (C) 2012 Alexandre Bezroutchko abb@gremwell.com
 # ----------------------------------------------------------------------
 import M2Crypto
+import logging
 
 IS_SSLv2_SUPPORTED = hasattr(M2Crypto.m2, "sslv2_method") and M2Crypto.m2.ssl_ctx_new(M2Crypto.m2.sslv2_method()) is not None
 
-PROTOCOLS = filter(None, ('sslv2' if IS_SSLv2_SUPPORTED else None, 'sslv3', 'tlsv1'))
+PROTOCOLS = ('sslv3', 'tlsv1')
+if IS_SSLv2_SUPPORTED:
+    PROTOCOLS += ('sslv2')
+else:
+    logging.getLogger('sslproto').fatal('Excluding SSLv2 from the list of tested protocol because OS does not support it')
+
 EXPORT_CIPHER = 'EXPORT'
 CIPHERS = ('HIGH', 'MEDIUM', 'LOW', EXPORT_CIPHER)
